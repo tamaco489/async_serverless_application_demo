a99d81688424ba5265956a30321199ad2ab203e0
takeuchima0 <miyabiii1210@gmail.com>
2024-12-01 00:38:01 +0900

    fix ecr

--- infra/ecr/ecr.tf
+++ infra/ecr/ecr.tf
@@ -135,7 +135,76 @@ resource "aws_ecr_lifecycle_policy" "ibis_api" {
 }
 
 # =================================================================
-# サムネイル画像生成サービス
+# ユーザ情報登録サービス
+# =================================================================
+resource "aws_ecr_repository" "coral_api" {
+  name = "${var.env}-coral-api"
+
+  # 既存のタグに対して、後から上書きを可能とする設定
+  image_tag_mutability = "MUTABLE"
+
+  # イメージがpushされる度に、自動的にセキュリティスキャンを行う設定を有効にする
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = { Name = "${var.env}-coral-api-ecr" }
+}
+
+# ライフサイクルポリシーの設定
+resource "aws_ecr_lifecycle_policy" "coral_api" {
+  repository = aws_ecr_repository.coral_api.name
+
+  policy = jsonencode(
+    {
+      "rules" : [
+        {
+          "rulePriority" : 1,
+          "description" : "バージョン付きのイメージを20個保持する、21個目がアップロードされた際には古いものから順に削除されていく",
+          "selection" : {
+            "tagStatus" : "tagged",
+            "tagPrefixList" : ["coral_v"],
+            "countType" : "imageCountMoreThan",
+            "countNumber" : 20
+          },
+          "action" : {
+            "type" : "expire"
+          }
+        },
+        {
+          "rulePriority" : 2,
+          "description" : "タグが設定されていないイメージをアップロードされてから3日後に削除する",
+          "selection" : {
+            "tagStatus" : "untagged",
+            "countType" : "sinceImagePushed",
+            "countUnit" : "days",
+            "countNumber" : 3
+          },
+          "action" : {
+            "type" : "expire"
+          }
+        },
+        {
+          "rulePriority" : 3,
+          "description" : "タグが設定されたイメージをアップロードされてから30日後に削除する",
+          "selection" : {
+            "tagStatus" : "any",
+            "countType" : "sinceImagePushed",
+            "countUnit" : "days",
+            "countNumber" : 30
+          },
+          "action" : {
+            "type" : "expire"
+          }
+        }
+      ]
+    }
+  )
+}
+
+
+# =================================================================
+# サムネイル画像生成バッチ
 # =================================================================
 resource "aws_ecr_repository" "image_maker_batch" {
   name = "${var.env}-image-maker-batch"
