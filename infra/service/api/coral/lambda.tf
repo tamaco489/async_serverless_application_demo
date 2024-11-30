99ff742166c1854f2a657f8791fe7e9c28d54a28
tamaco489 <tamaco489@gmail.com>
2024-12-01 02:33:56 +0900

    add api gateway for coral api server

--- infra/service/api/coral/lambda.tf
+++ infra/service/api/coral/lambda.tf
@@ -1,6 +1,6 @@
 resource "aws_lambda_function" "coral_api" {
   function_name = "${var.env}-coral-api"
-  description   = "ユーザ情報登録APIサーバ"
+  description   = "ユーザ情報管理APIサーバ"
   role          = aws_iam_role.coral_api.arn
   package_type  = "Image"
   image_uri     = "${data.terraform_remote_state.ecr.outputs.coral_api.url}:coral_v0.0.0"
@@ -20,4 +20,13 @@ resource "aws_lambda_function" "coral_api" {
   }
 
   tags = { Name = "${var.env}-coral-api" }
+}
+
+# Lambda 関数が API Gateway から呼び出せるようにする権限
+resource "aws_lambda_permission" "api_gateway_invoke" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.coral_api.arn
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.coral_api_http.execution_arn}/*/*"
 }
