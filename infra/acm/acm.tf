e82ac013adda4de24ebe964b45b6f0d79895efc2
tamaco489 <tamaco489@gmail.com>
2024-12-01 02:24:44 +0900

    fix acm

--- infra/acm/acm.tf
+++ infra/acm/acm.tf
@@ -10,8 +10,8 @@ resource "aws_acm_certificate" "nautilus" {
   tags = { Name = "${local.fqn}-acm" }
 }
 
-# API Gatewayのカスタムドメインの定義
-resource "aws_apigatewayv2_domain_name" "api_http_v2" {
+# API Gatewayのカスタムドメインの定義（画像処理サービスAPI向け）
+resource "aws_apigatewayv2_domain_name" "ibis_api_http_v2" {
   domain_name = "apiv2.${data.terraform_remote_state.route53.outputs.host_zone.name}"
 
   domain_name_configuration {
@@ -21,15 +21,15 @@ resource "aws_apigatewayv2_domain_name" "api_http_v2" {
   }
 }
 
-# API V2向けのRoute53 Aレコードを設定
-resource "aws_route53_record" "api" {
+# API V2向けのRoute53 Aレコードを設定（画像処理サービスAPI向け）
+resource "aws_route53_record" "ibis_api_http_v2" {
   zone_id = data.terraform_remote_state.route53.outputs.host_zone.id
   name    = "apiv2.${data.terraform_remote_state.route53.outputs.host_zone.name}"
   type    = "A"
 
   alias {
-    name                   = aws_apigatewayv2_domain_name.api_http_v2.domain_name_configuration.0.target_domain_name
-    zone_id                = aws_apigatewayv2_domain_name.api_http_v2.domain_name_configuration.0.hosted_zone_id
+    name                   = aws_apigatewayv2_domain_name.ibis_api_http_v2.domain_name_configuration.0.target_domain_name
+    zone_id                = aws_apigatewayv2_domain_name.ibis_api_http_v2.domain_name_configuration.0.hosted_zone_id
     evaluate_target_health = false
   }
 }
