1623f807d6c19d61b409897ea7ebc3a2fd549fd1
tamaco489 <tamaco489@gmail.com>
2024-12-01 02:33:29 +0900

    fix acm for v3 record

--- infra/acm/outputs.tf
+++ infra/acm/outputs.tf
@@ -16,3 +16,14 @@ output "ibis_apigatewayv2_domain_name" {
   }
   description = "Details of the API Gateway v2 custom domain name configuration."
 }
+
+output "coral_apigatewayv2_domain_name" {
+  value = {
+    id              = aws_apigatewayv2_domain_name.coral_api_http_v3.id
+    domain_name     = aws_apigatewayv2_domain_name.coral_api_http_v3.domain_name
+    endpoint_type   = aws_apigatewayv2_domain_name.coral_api_http_v3.domain_name_configuration[0].endpoint_type
+    security_policy = aws_apigatewayv2_domain_name.coral_api_http_v3.domain_name_configuration[0].security_policy
+    certificate_arn = aws_apigatewayv2_domain_name.coral_api_http_v3.domain_name_configuration[0].certificate_arn
+  }
+  description = "Details of the API Gateway v2 custom domain name configuration."
+}
