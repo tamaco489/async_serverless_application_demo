b38a8345bdb0ab57455fbe763a01de91a68a0091
tamaco489 <tamaco489@gmail.com>
2024-12-01 19:31:22 +0900

    fix coral api lambda iam role

--- infra/service/api/coral/iam.tf
+++ infra/service/api/coral/iam.tf
@@ -19,6 +19,27 @@ resource "aws_iam_role" "coral_api" {
   tags               = { Name = "${var.env}-coral-api-iam-role" }
 }
 
+# DynamoDBへのアクセス権を定義するポリシードキュメントを生成する
+data "aws_iam_policy_document" "coral_api" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "dynamodb:PutItem",
+      "dynamodb:UpdateItem",
+      "dynamodb:BatchWriteItem"
+    ]
+    resources = ["${aws_dynamodb_table.user_table.arn}"]
+  }
+}
+
+# DynamoDBアクセス権限をIAM Roleに付与する
+resource "aws_iam_role_policy" "coral_api" {
+  name   = "${var.env}-coral-api-dynamodb-access-policy"
+  role   = aws_iam_role.coral_api.id
+  policy = data.aws_iam_policy_document.coral_api.json
+}
+
+
 # 予め定義しておいた、Lambda共通のポリシー（ログ関連）をアタッチする
 resource "aws_iam_role_policy_attachment" "coral_api_logs" {
   policy_arn = data.terraform_remote_state.lambda.outputs.iam.lambda_logging_policy_arn
