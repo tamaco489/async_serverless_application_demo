2aa655d964407cabf273710922dd322cddddf520
tamaco489 <tamaco489@gmail.com>
2024-12-14 21:05:18 +0900

    fix get user by id handler pre

--- api/coral/intrenal/handler/users.go
+++ api/coral/intrenal/handler/users.go
@@ -3,7 +3,10 @@ package handler
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func (ch *CoralHandler) CreateUserHandler(w http.ResponseWriter, r *http.Request) {
@@ -46,7 +49,7 @@ func (ch *CoralHandler) GetMeHandler(w http.ResponseWriter, r *http.Request) {
 	// 本来であれば、middlewareでhttp headerに設定されたjwtの正当性検証を行い、その過程を経て以降の処理を行うのが望ましい
 
 	// ここではこのuidは正当なユーザのuidであるものとして扱う
-	uid := "0193c4cc-1e5c-7188-ab2f-3856a4b73566"
+	uid := "0193c504-2b40-79c6-8a35-acfe1e938ac5"
 
 	response, err := ch.userUseCase.GetMeUser(r.Context(), uid)
 	if err != nil {
@@ -59,5 +62,38 @@ func (ch *CoralHandler) GetMeHandler(w http.ResponseWriter, r *http.Request) {
 	if err := json.NewEncoder(w).Encode(response); err != nil {
 		http.Error(w, fmt.Sprintf("failed to encode response: %v", err), http.StatusInternalServerError)
 		return
+	}
+}
+
+func (ch *CoralHandler) GetUserByIDHandler(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+	userID := vars["userID"]
+
+	if userID == "" {
+		http.Error(w, "userID is required", http.StatusBadRequest)
+		return
+	}
+
+	switch r.Method {
+	case http.MethodGet:
+		log.Println("[debug] GetUserByIDHandler userID:", userID, "method:", r.Method)
+		//
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		response := map[string]string{"user_id": userID}
+		if err := json.NewEncoder(w).Encode(response); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+	case http.MethodPut:
+		//
+
+	case http.MethodDelete:
+		//
+
+	default:
+		http.Error(w, "Method not supported", http.StatusMethodNotAllowed)
 	}
 }
